3dade84c0192cd31890413418866cc3549e55f27
WaiLinnAung18 <waia00198@gmail.com>
2023-05-13 15:50:09 +0630

    merge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { ActionIcon, Avatar, Menu, TextInput } from "@mantine/core";
 
 import React from "react";
 import { BsSearch } from "react-icons/bs";
-import { BiBookContent, BiChevronRight } from "react-icons/bi";
+import { BiChevronRight } from "react-icons/bi";
 import { IoMdNotificationsOutline } from "react-icons/io";
 const Navbar = () => {
   return (
@@ -23,42 +23,41 @@ const Navbar = () => {
           placeholder="Search questions"
           rightSectionWidth={42}
         />
-
-        <div className="flex gap-5 items-center">
-          <div>
-            <IoMdNotificationsOutline className="text-2xl" />
-          </div>
-          <Menu
-            width={200}
-            withArrow
-            className="hover:bg-slate-100 p-2 cursor-pointer"
-          >
-            <Menu.Target>
-              <div className="flex gap-3 items-center">
-                <Avatar
-                  radius="xl"
-                  src={
-                    "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-                  }
-                />
-                <div className="flex flex-col">
-                  <p className="font-semibold">Wai Linn Aung</p>
-                  <p className="text-sm text-slate-500">Frontend developer</p>
-                </div>
-                <BiChevronRight className="text-2xl" />
-              </div>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item component="a" href="#">
-                Mantine website
-              </Menu.Item>
-
-              <Menu.Item component="a" href="#" target="_blank">
-                External link
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
+      </div>
+      <div className="flex gap-5 items-center">
+        <div>
+          <IoMdNotificationsOutline className="text-3xl" />
         </div>
+        <Menu
+          width={200}
+          withArrow
+          className="hover:bg-slate-100 p-2 cursor-pointer"
+        >
+          <Menu.Target>
+            <div className="flex gap-3 items-center">
+              <Avatar
+                radius="xl"
+                src={
+                  "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+                }
+              />
+              <div className="flex flex-col">
+                <p className="font-semibold">Wai Linn Aung</p>
+                <p className="text-sm text-slate-500">Frontend developer</p>
+              </div>
+              <BiChevronRight className="text-2xl" />
+            </div>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item component="a" href="#">
+              Mantine website
+            </Menu.Item>
+
+            <Menu.Item component="a" href="#" target="_blank">
+              External link
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </div>
     </div>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const SideBar = () => {
   return (
-    <div className="w-[400px] bg-slate-50 h-[3000px] my-5">
-      <div className="flex flex-col gap-2">
-        <Link to="/" className="py-2 px-8 bg-slate-300 mx-5">
-          home
-        </Link>
-        <Link to="/blog" className="py-2 px-8 bg-slate-300 mx-5">
-          blog
-        </Link>
-        <p className="py-2 px-8 bg-slate-300 mx-5">team</p>
-      </div>
+    <div className="col-span-2 h-screen bg-slate-300">
+      <div className="flex flex-col items-center"></div>
     </div>
   );
 };
